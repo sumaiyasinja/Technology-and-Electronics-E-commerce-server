abd349ab39a1e3ced6d55eba8ce06add81d17f16
Sumaiya Sinja <sumiyasinja@gmail.com>
2024-02-22 13:12:36 +0600

    mongodb connected, vercel deplopment added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require("mongodb");
 var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000
 
 // middleware
 app.use(cors())
+app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctrkbrk.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+
+async function run() {
+  try {
+    await client.connect();
+    // databse collections
+
+    const database = client.db("BytopiaTechShopDB");
+    const ProductCollection = database.collection("ProductCollection");
+    
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
